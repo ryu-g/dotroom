03e9938fc8113527973104eaeb8126797899182b
ryu-g <b1013178@fun.ac.jp>
2017-11-29 14:40:13 +0900

    changed init position

--- canvas/canvas.js
+++ canvas/canvas.js
@@ -2,17 +2,18 @@ function draw(){
 	var canvas = document.getElementById('canvas');
 	console.log("hello");
 	if (canvas.getContext){
+		var width = canvas.width;
+		var height = canvas.height;
 		var ctx = canvas.getContext('2d');
 		//----仮置きのuserinfo----
 		var unitImage= new Image();
 		unitImage.src= "./unit/unit0001.png";
-		var x = 0;
-		var y = 0;
+		var x = width/2;
+		var y = height/2;
 		//----
-		var width = canvas.width;
-		var height = canvas.height;
 		var increase = 10;
 		initUnit(x,y);
+		drawUnit(x,y);
 		document.onkeydown = function (e){
 			if(!e) e = window.event;
 			var keyCode =e.keyCode;
@@ -43,6 +44,7 @@ function draw(){
 	function initUnit(x,y){
 		ctx.clearRect(0, 0, width, height);
 		ctx.drawImage(unitImage, x, y);	
+		console.log("initialized")
 	}
 	function drawUnit(x,y){
 		ctx.clearRect(0, 0, width, height);
