24f694db7280c91f1db2d6014d5691fcee64393c
ryu-g <b1013178@fun.ac.jp>
2017-11-30 23:16:26 +0900

    server no Authoring kiso dekisou

--- canvas/server.js
+++ canvas/server.js
@@ -1,8 +1,8 @@
-var app  = require('http');
-var fs   = require('fs');
-var path = require('path');
-var io   = require('socket.io').listen(app);
-var PORT = 1337;
+var app = require('http').createServer(handler);
+var fs  = require('fs');
+var path= require('path');
+var io  = require('socket.io').listen(app);
+var PORT= 1337;
 var mime = {
   ".html": "text/html",
   ".css":  "text/css",
@@ -10,36 +10,42 @@ var mime = {
   // 読み取りたいMIMEタイプはここに追記
 };
 
-var http_server = new app.createServer(function(req, res) {
-  if (req.url == '/') {
-    filePath = '/canvas.html';
-  } else {
-    filePath = req.url;
-  }
-  var fullPath = __dirname + filePath;
-
-  res.writeHead(200, {"Content-Type": mime[path.extname(fullPath)] || "text/plain"});
-  fs.readFile(fullPath, function(err, data) {
-    if (err) {
-    	console.log(err);
-    } else {
-    	res.end(data, 'UTF-8');
-    }
-  });
-}).listen(PORT);
+app.listen(1337);//seerverを待ち受け状態にするよ
 console.log(`Server running at http://localhost:/${PORT}`);
 
-io.sockets.on('connection',function(socket){
-  socket.on('login', function(data){
-    socket.client_id = data.id;
-    socket.client_pw = data.pw;
-    console.log('login check unit ID: '+data.id+', unit pw: '+data.pw);
-    io.sockets.emit('auth_from_server','['+socket.client_name+']:' +data.msg)
+function handler(req,res){
+  fs.readFile(__dirname + '/canvas.html', function(err, data){
+    if (req.url == '/') {
+      filePath = '/canvas.html'; 
+       } else {
+      filePath = req.url;
+      }
+      var fullPath = __dirname + filePath;
+
+    res.writeHead(200,{"Content-type":mime[path.extname(fullPath)] || "text/plain" });
+    fs.readFile(fullPath, function(err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.end(data, 'UTF-8');
+    };
+    res.end();
+    })
   });
-  socket.on('signin', function(data){
-    socket.client_id = data.name;
-    socket.client_id = data.pw;
-    console.log('unit ID: '+data.name+', unit name: '+data.pw);
-    io.sockets.emit('auth_from_server','['+socket.client_name+']:' +data.msg)
+}
+
+//socketですよ
+io.sockets.on('connection',function(socket){
+  socket.on('emit_login', function(data){
+    socket.client_id = data.unitId;
+    socket.client_pw = data.unitPass;
+    console.log('login check unit ID: '+socket.client_id+', unit pass: '+socket.client_pw);
+    // io.sockets.emit('auth_from_server','['+socket.client_name+']:' +data.msg)
+  });
+  socket.on('emit_signin', function(data){
+    socket.client_name = data.unitName;
+    socket.client_pw = data.unitPass;
+    console.log('unit ID: '+socket.client_name+', unit pass: '+socket.client_pw);
+    // io.sockets.emit('auth_from_server','['+socket.client_name+']:' +data.msg)
   });
 });
